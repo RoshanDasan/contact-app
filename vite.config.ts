89fa69f10b7d436d3f986afc0744c07fc150a6ca
RoshanDasan <edroshan2001@gmail.com>
2023-06-02 13:44:28 +0530

    build-done

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/contact-app/"
-})
+  base: '/contact-app/',
+  server: {
+    headers: {
+      'Permissions-Policy': '',
+    },
+  },
+});
